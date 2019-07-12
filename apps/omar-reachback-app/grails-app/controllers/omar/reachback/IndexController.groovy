9245483e0c6f8244cd0eaf59a9e989c191584731
knaquin <kevin.naquin@digitalglobe.com>
2019-07-12 10:37:18 -0400

    Adding swagger docs.

--- apps/omar-reachback-app/grails-app/controllers/omar/reachback/IndexController.groovy
+++ apps/omar-reachback-app/grails-app/controllers/omar/reachback/IndexController.groovy
@@ -1,5 +1,17 @@
 package omar.reachback
 
+
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiImplicitParam
+import io.swagger.annotations.ApiImplicitParams
+import io.swagger.annotations.ApiOperation
+
+
+@Api(value = "/index",
+	 description = "OMAR Reachback REST API"
+)
+
+
 class IndexController {
 
 	def searchService
@@ -9,6 +21,56 @@ class IndexController {
 		render "N/A"
 	}
 
+
+	@ApiOperation(
+		httpMethod = "GET",
+		produces = 'json',
+		value = "Set search parameters with the URL."
+	)
+	@ApiImplicitParams([
+		@ApiImplicitParam(
+			dataType = 'string',
+			defaultValue = '',
+			name = 'bbox',
+			paramType = 'query',
+			value = 'Sets the boundes fpr the search in min_lon, min_lat, max_lon, max_lat format.'
+		),
+		@ApiImplicitParam(
+			dataType = 'string',
+			defaultValue = '',
+			name = 'endDate',
+			paramType = 'query',
+			value = 'Sets the end date for the search.'
+		),
+		@ApiImplicitParam(
+			dataType = 'integer',
+			defaultValue = '',
+			name = 'maxResults',
+			paramType = 'query',
+			value = 'Sets the maximum results returned.'
+		),
+		@ApiImplicitParam(
+			dataType = 'integer',
+			defaultValue = '',
+			name = 'niirs',
+			paramType = 'query',
+			value = 'Sets the minimum NIIRS for the search.'
+		),
+		@ApiImplicitParam(
+			dataType = 'string',
+			defaultValue = '',
+			name = 'sensors',
+			paramType = 'query',
+			value = 'Comma separated list of sensor IDs.'
+		),
+		@ApiImplicitParam(
+			dataType = 'string',
+			defaultValue = '',
+			name = 'startDate',
+			paramType = 'query',
+			value = 'Sets the start date for the search.'
+		)
+	])
 	def search() {
 		def results = searchService.search( params )
 
