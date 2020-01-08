bcadc5c8646be99e607367922211bb419b3e2f16
knaquin <kevin.naquin@digitalglobe.com>
2020-01-08 13:15:32 -0700

    Syntax error.

--- apps/omar-reachback-app/grails-app/services/omar/reachback/SearchService.groovy
+++ apps/omar-reachback-app/grails-app/services/omar/reachback/SearchService.groovy
@@ -55,7 +55,7 @@ class SearchService {
             image.save()
 
             if ( image.hasErrors() ) {
-                images.errors.allErrors.each { println it }
+                image.errors.allErrors.each { println it }
             }
         }
     }
